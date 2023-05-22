de684fc4df2a1c21576ff6ffed9611daab55645b
Danny Mishler <dsmishler@icl.utk.edu>
2023-05-22 13:42:52 -0400

    testing this fix to restore GEMM tasks

--- parsec/interfaces/ptg/ptg-compiler/jdf2c.c
+++ parsec/interfaces/ptg/ptg-compiler/jdf2c.c
@@ -861,6 +861,15 @@ static char *dump_profiling_init(void **elem, void *arg)
     string_arena_t *profiling_convertor_params;
 
     if( !profile_enabled(f->properties) ) {
+        /**
+         * We have to set the `profiling_array` elements to -1
+         * then we will return NULL
+         */
+        string_arena_add_string(info->sa,
+                                "%s_profiling_array[START_KEY(%s_%s.task_class_id + PARSEC_%s_NB_TASK_CLASSES) /* %s (internal init) start key */] = -1;\n"
+                                "%s_profiling_array[END_KEY(%s_%s.task_class_id + PARSEC_%s_NB_TASK_CLASSES)   /* %s (internal init) end   key */] = -1;\n",
+                                jdf_basename, jdf_basename, fname, jdf_basename, fname,
+                                jdf_basename, jdf_basename, fname, jdf_basename, fname);
         return NULL;
     }
 
@@ -885,15 +894,15 @@ static char *dump_profiling_init(void **elem, void *arg)
                             jdf_basename, fname, 256-R, 256-G, 256-B,
                             jdf_basename, fname, jdf_basename, fname,
                             jdf_basename, fname, jdf_basename, fname);
-    string_arena_add_string(info->sa,
-                            "%s_profiling_array_counter = 0; /* initialize profiling array to -1 */\n"
-                            "for(; %s_profiling_array_counter < %s_profiling_array_counter_limit; %s_profiling_array_counter++)\n"
-                            "{\n"
-                            "    %s_profiling_array[%s_profiling_array_counter] = -1;\n"
-                            "}\n",
-                            jdf_basename,
-                            jdf_basename, jdf_basename, jdf_basename, 
-                            jdf_basename, jdf_basename);
+    // string_arena_add_string(info->sa,
+                            // "%s_profiling_array_counter = 0; /* initialize profiling array to -1 */\n"
+                            // "for(; %s_profiling_array_counter < %s_profiling_array_counter_limit; %s_profiling_array_counter++)\n"
+                            // "{\n"
+                            // "    %s_profiling_array[%s_profiling_array_counter] = -1;\n"
+                            // "}\n",
+                            // jdf_basename,
+                            // jdf_basename, jdf_basename, jdf_basename, 
+                            // jdf_basename, jdf_basename);
     string_arena_add_string(info->sa,
                             "parsec_profiling_add_dictionary_keyword(\"%s::%s\", \"fill:%02X%02X%02X\",\n"
                             "                                       sizeof(parsec_task_prof_info_t)+%d*sizeof(parsec_assignment_t),\n"
@@ -1596,18 +1605,12 @@ static void jdf_generate_structure(jdf_t *jdf)
         coutput("#if defined(PARSEC_PROF_TRACE)\n"
                 "#  if defined(PARSEC_PROF_TRACE_PTG_INTERNAL_INIT)\n"
                 "static int %s_profiling_array[4*PARSEC_%s_NB_TASK_CLASSES] = {-1}; /* 2 pairs (begin, end) per task, times two because each task class has an internal_init task */\n"
-                "static int %s_profiling_array_counter_limit = 4*PARSEC_%s_NB_TASK_CLASSES;\n"
                 "#  else /* defined(PARSEC_PROF_TRACE_PTG_INTERNAL_INIT) */\n"
                 "static int %s_profiling_array[2*PARSEC_%s_NB_TASK_CLASSES] = {-1}; /* 2 pairs (begin, end) per task */\n"
-                "static int %s_profiling_array_counter_limit = 2*PARSEC_%s_NB_TASK_CLASSES;\n"
                 "#  endif /* defined(PARSEC_PROF_TRACE_PTG_INTERNAL_INIT) */\n"
-                "static int %s_profiling_array_counter;\n"
                 "#endif  /* defined(PARSEC_PROF_TRACE) */\n",
                 jdf_basename, jdf_basename,
-                jdf_basename, jdf_basename,
-                jdf_basename, jdf_basename,
-                jdf_basename, jdf_basename,
-                jdf_basename);
+                jdf_basename, jdf_basename);
 
     UTIL_DUMP_LIST(sa1, jdf->globals, next,
                    dump_globals, sa2, "", "#define ", "\n", "\n");
